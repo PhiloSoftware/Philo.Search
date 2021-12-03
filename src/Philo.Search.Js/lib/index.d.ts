e36a10a9fb1cdf3d386502269acc5bab03dad760
Morgan Philo <morgan@philo.co.nz>
2021-12-03 19:21:35 +1300

    remove build files

--- src/Philo.Search.Js/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { DataColumn, DataColumnFilterValue, Filter, FilterGroup, FilterSet, SortDirection } from "./datastructure";
-import Processor from "./processor";
-export { DataColumn, DataColumnFilterValue, Filter, FilterGroup, FilterSet, SortDirection, Processor };
--- src/Philo.Search.Js/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Processor = exports.SortDirection = void 0;
-var datastructure_1 = require("./datastructure");
-Object.defineProperty(exports, "SortDirection", { enumerable: true, get: function () { return datastructure_1.SortDirection; } });
-var processor_1 = require("./processor");
-exports.Processor = processor_1.default;
