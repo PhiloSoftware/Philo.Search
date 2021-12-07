16b5c1c5b93cb09b49cf2f6f255f355ef1a0115b
Jarrod Lodge <jarrod@limedigital.nz>
2021-12-08 11:31:55 +1300

    Working with rollup and npm

---
+++ src/JarrodTest/rollup.config.js
@@ -0,0 +1,25 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import scss from 'rollup-plugin-scss'
+import vue from "rollup-plugin-vue";
+
+import packageJson from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+  plugins: [ vue(), peerDepsExternal(), resolve(), commonjs(), typescript(), scss()]
+};
---
+++ src/JarrodTest/src/index.ts
@@ -0,0 +1,4 @@
+import SampleComponent from "./SampleComponent/SampleComponent.vue";
+import Table from "./Table/Table.vue";
+
+export { SampleComponent, Table };
