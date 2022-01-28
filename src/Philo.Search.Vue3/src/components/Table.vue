fd8e4641cff547cb5043aea90bb08678570290f2
morganphilo <morgan@philo.co.nz>
2022-01-28 19:01:29 +1300

    Rotate the reload button when loading

--- src/Philo.Search.Vue3/src/components/Table.vue
+++ src/Philo.Search.Vue3/src/components/Table.vue
@@ -147,6 +147,7 @@ const loadData = async (query: {
     total: res.totalRowCount,
     per_page: filter.pageSize,
   }
+  fetchingData.value = false;
 };
 const loadDataWithLastQuery = async() => {
   return loadData({
@@ -263,136 +264,145 @@ if (route) {
             Filters
           </button>
         </div>
-        <transition name="slide-down">
-          <div v-if="showFilter" :class="colClass">
-            <div :class="rowClass" class="tableFilters">
-              <div
-                :class="colClass"
-                v-for="filter in processor.columnFilters"
-                v-bind:key="filter.id"
-              >
-                <template v-if="filter.options">
-                  <slot
-                    name="field-select" 
-                    v-bind="{ filter, change: requestDataLoad }"
-                  >
-                    <div class="label">{{ filter.label }}</div>
-                    <select
-                      v-model="filter.value"
-                      :name="filter.id"
-                      :multiple="filter.props.multiple"
-                      @change="requestDataLoad"
+        <div :class="colClass">
+          <div :class="rowClass">
+            <div :class="colClass">
+              <transition name="slide-down">
+                <div v-if="showFilter" :class="colClass">
+                  <div :class="rowClass" class="tableFilters">
+                    <div
+                      :class="colClass"
+                      v-for="filter in processor.columnFilters"
+                      v-bind:key="filter.id"
                     >
-                      <option
-                        v-for="option in filter.options"
-                        :key="option.value"
-                        :value="option.value"
+                      <template v-if="filter.options">
+                        <slot
+                          name="field-select" 
+                          v-bind="{ filter, change: requestDataLoad }"
+                        >
+                          <div class="label">{{ filter.label }}</div>
+                          <select
+                            v-model="filter.value"
+                            :name="filter.id"
+                            :multiple="filter.props.multiple"
+                            class="w-100"
+                            @change="requestDataLoad"
+                          >
+                            <option
+                              v-for="option in filter.options"
+                              :key="option.value"
+                              :value="option.value"
+                            >
+                              {{ option.label }}
+                            </option>
+                          </select>
+                        </slot>
+                      </template>
+                      <template v-else-if="filter.type === columnFilterType.number">
+                        <slot
+                          name="field-number" 
+                          v-bind="{ filter, change: requestDataLoad }"
+                        >
+                          <div class="label">{{ filter.label }}</div>
+                          <input
+                            type="number"
+                            v-model="filter.value"
+                            :min="filter.props.min"
+                            :max="filter.props.max"
+                            :step="filter.props.step"
+                            @change="requestDataLoad"
+                          />
+                        </slot>
+                      </template>
+                      <template v-else-if="filter.type === columnFilterType.bool">
+                        <slot
+                          name="field-bool" 
+                          v-bind="{ filter, change: requestDataLoad }"
+                        >
+                          <div class="label">{{ filter.label }}</div>
+                          <select
+                            v-model="filter.value"
+                            :name="filter.id"
+                            @change="requestDataLoad"
+                          >
+                            <option
+                              v-if="filter.nullable === true"
+                              :value="null"
+                            ></option>
+                            <option :value="true">Yes</option>
+                            <option :value="false">No</option>
+                          </select>
+                        </slot>
+                      </template>
+                      <template
+                        v-else-if="
+                          filter.type === 'date' || filter.type === 'unixdate'
+                        "
                       >
-                        {{ option.label }}
-                      </option>
-                    </select>
-                  </slot>
-                </template>
-                <template v-else-if="filter.type === columnFilterType.number">
-                  <slot
-                    name="field-number" 
-                    v-bind="{ filter, change: requestDataLoad }"
-                  >
-                    <div class="label">{{ filter.label }}</div>
-                    <input
-                      type="number"
-                      v-model="filter.value"
-                      :min="filter.props.min"
-                      :max="filter.props.max"
-                      :step="filter.props.step"
-                      @change="requestDataLoad"
-                    />
-                  </slot>
-                </template>
-                <template v-else-if="filter.type === columnFilterType.bool">
-                  <slot
-                    name="field-bool" 
-                    v-bind="{ filter, change: requestDataLoad }"
-                  >
-                    <div class="label">{{ filter.label }}</div>
-                    <select
-                      v-model="filter.value"
-                      :name="filter.id"
-                      @change="requestDataLoad"
-                    >
-                      <option
-                        v-if="filter.nullable === true"
-                        :value="null"
-                      ></option>
-                      <option :value="true">Yes</option>
-                      <option :value="false">No</option>
-                    </select>
-                  </slot>
-                </template>
-                <template
-                  v-else-if="
-                    filter.type === 'date' || filter.type === 'unixdate'
-                  "
-                >
-                  <slot
-                    name="field-date" 
-                    v-bind="{ filter, change: requestDataLoad }"
-                  >
-                    <div class="label">{{ filter.label }}</div>
-                    <input
-                      type="date"
-                      v-model="filter.value"
-                      :min="filter.props.min"
-                      :max="filter.props.max"
-                      @change="requestDataLoad"
-                    />
-                  </slot>
-                </template>
-                <template v-else-if="filter.type === columnFilterType.text">
-                  <slot
-                    name="field-text" 
-                    v-bind="{ filter, change: requestDataLoad }"
-                  >
-                    <div class="label">{{ filter.label }}</div>
-                    <input
-                      type="text"
-                      v-model="filter.value"
-                      @keyup="requestDataLoad"
-                    />
-                  </slot>
-                </template>
-                <template v-else-if="filter.type === columnFilterType.guid">
-                  <slot
-                    name="field-guid" 
-                    v-bind="{ filter, change: requestDataLoad }"
-                  >
-                    <div class="label">{{ filter.label }}</div>
-                    <input
-                      type="text"
-                      v-model="filter.value"
-                      @keyup="requestDataLoad"
-                    />
-                  </slot>
-                </template>
-                <template v-else>
-                  <slot
-                    name="field-default" 
-                    v-bind="{ filter, change: requestDataLoad }"
-                  >
-                    <div class="label">{{ filter.label }}</div>
-                    <input
-                      type="text"
-                      v-model="filter.value"
-                      @keyup="requestDataLoad"
-                    />
-                  </slot>
-                </template>
-              </div>
+                        <slot
+                          name="field-date" 
+                          v-bind="{ filter, change: requestDataLoad }"
+                        >
+                          <div class="label">{{ filter.label }}</div>
+                          <input
+                            type="date"
+                            v-model="filter.value"
+                            :min="filter.props.min"
+                            :max="filter.props.max"
+                            @change="requestDataLoad"
+                          />
+                        </slot>
+                      </template>
+                      <template v-else-if="filter.type === columnFilterType.text">
+                        <slot
+                          name="field-text" 
+                          v-bind="{ filter, change: requestDataLoad }"
+                        >
+                          <div class="label">{{ filter.label }}</div>
+                          <input
+                            type="text"
+                            v-model="filter.value"
+                            @keyup="requestDataLoad"
+                          />
+                        </slot>
+                      </template>
+                      <template v-else-if="filter.type === columnFilterType.guid">
+                        <slot
+                          name="field-guid" 
+                          v-bind="{ filter, change: requestDataLoad }"
+                        >
+                          <div class="label">{{ filter.label }}</div>
+                          <input
+                            type="text"
+                            v-model="filter.value"
+                            @keyup="requestDataLoad"
+                          />
+                        </slot>
+                      </template>
+                      <template v-else>
+                        <slot
+                          name="field-default" 
+                          v-bind="{ filter, change: requestDataLoad }"
+                        >
+                          <div class="label">{{ filter.label }}</div>
+                          <input
+                            type="text"
+                            v-model="filter.value"
+                            @keyup="requestDataLoad"
+                          />
+                        </slot>
+                      </template>
+                    </div>
+                  </div>
+                </div>
+              </transition>
+              
             </div>
           </div>
-        </transition>
+        </div>
         <div :class="colClass" class="t-tiny-col t-ml-auto t-text-right">
           <div class="reload">
+            <div :class="{ rotating: fetchingData }">
             <svg
               aria-hidden="true"
               focusable="false"
@@ -401,7 +411,7 @@ if (route) {
               role="img"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 512 512"
-              :class="{ rotating: fetchingData }"
+              
               @click="requestDataLoad"
             >
               <path
@@ -409,6 +419,7 @@ if (route) {
                 d="M370.72 133.28C339.458 104.008 298.888 87.962 255.848 88c-77.458.068-144.328 53.178-162.791 126.85-1.344 5.363-6.122 9.15-11.651 9.15H24.103c-7.498 0-13.194-6.807-11.807-14.176C33.933 94.924 134.813 8 256 8c66.448 0 126.791 26.136 171.315 68.685L463.03 40.97C478.149 25.851 504 36.559 504 57.941V192c0 13.255-10.745 24-24 24H345.941c-21.382 0-32.09-25.851-16.971-40.971l41.75-41.749zM32 296h134.059c21.382 0 32.09 25.851 16.971 40.971l-41.75 41.75c31.262 29.273 71.835 45.319 114.876 45.28 77.418-.07 144.315-53.144 162.787-126.849 1.344-5.363 6.122-9.15 11.651-9.15h57.304c7.498 0 13.194 6.807 11.807 14.176C478.067 417.076 377.187 504 256 504c-66.448 0-126.791-26.136-171.315-68.685L48.97 471.03C33.851 486.149 8 475.441 8 454.059V320c0-13.255 10.745-24 24-24z"
               ></path>
             </svg>
+            </div>  
           </div>
         </div>
       </div>
@@ -430,7 +441,6 @@ if (route) {
 <style src="@jobinsjp/vue3-datatable/dist/style.css">
 </style>
 <style scoped>
-
 .t-row {
   display: flex;
   width: 100%;
@@ -457,38 +467,21 @@ if (route) {
   display: inline-block;
   margin-bottom: 0.4em;
 }
-.reload svg.rotating {
-  @keyframes rotating {
-    from {
-      transform: rotate(0deg);
-      -o-transform: rotate(0deg);
-      -ms-transform: rotate(0deg);
-      -moz-transform: rotate(0deg);
-      -webkit-transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-      -o-transform: rotate(360deg);
-      -ms-transform: rotate(360deg);
-      -moz-transform: rotate(360deg);
-      -webkit-transform: rotate(360deg);
-    }
-  }
-  @-webkit-keyframes rotating {
-    from {
-      transform: rotate(0deg);
-      -webkit-transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-      -webkit-transform: rotate(360deg);
-    }
-  }
-  -webkit-animation: rotating 2s linear infinite;
-  -moz-animation: rotating 2s linear infinite;
-  -ms-animation: rotating 2s linear infinite;
-  -o-animation: rotating 2s linear infinite;
-  animation: rotating 2s linear infinite;
+
+@-moz-keyframes spin { 
+    100% { -moz-transform: rotate(360deg); } 
+}
+@-webkit-keyframes spin { 
+    100% { -webkit-transform: rotate(360deg); } 
+}
+@keyframes spin { 
+    100% { 
+        -webkit-transform: rotate(360deg); 
+        transform:rotate(360deg); 
+    } 
+}
+.reload .rotating {
+  animation: spin 2s linear infinite;
 }
 
 .slide-down-enter-active {
@@ -523,5 +516,8 @@ if (route) {
 .slide-down-leave-to {
   overflow: hidden;
   max-height: 0;
+}
+.w-100 {
+  width: 100%;
 }
 </style>
